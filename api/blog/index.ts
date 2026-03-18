b43d7f0e1fb021709e64c2397fc8abab51aca98b
CardanoBuyBot <tonscanner@outlook.com>
2026-03-18 20:15:01 -0300

    Fix import path for getPool in index.ts

--- api/blog/index.ts
+++ api/blog/index.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { getPool } from '../lib/db.js';
+import { getPool } from '../../lib/db.js';
 
 type BlogListItem = {
   slug: string;
