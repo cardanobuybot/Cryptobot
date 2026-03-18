383c7b0e0e87257451d632273f830a6536b5548f
CardanoBuyBot <tonscanner@outlook.com>
2026-03-18 20:21:50 -0300

    Include lastmod in sitemap for published articles
    Updated sitemap generation to include last modified dates for articles.

--- api/sitemap.ts
+++ api/sitemap.ts
@@ -5,22 +5,30 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   const pool = getPool();
 
   const result = await pool.query(`
-    SELECT slug FROM articles
+    SELECT slug, created_at
+    FROM articles
     WHERE status = 'published'
   `);
 
-  const urls = result.rows.map(a => `
-    <url>
-      <loc>https://cryptobot.ltd/blog/${a.slug}</loc>
-    </url>
-  `).join('');
+  const urls = result.rows.map(a => {
+    const lastmod = new Date(a.created_at).toISOString();
+    return `
+      <url>
+        <loc>https://cryptobot.ltd/blog/${a.slug}</loc>
+        <lastmod>${lastmod}</lastmod>
+      </url>
+    `;
+  }).join('');
 
   res.setHeader('Content-Type', 'application/xml');
   res.send(`<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <url>
-      <loc>https://cryptobot.ltd/blog</loc>
-    </url>
-    ${urls}
-  </urlset>`);
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+
+  <url>
+    <loc>https://cryptobot.ltd/blog</loc>
+  </url>
+
+  ${urls}
+
+</urlset>`);
 }
