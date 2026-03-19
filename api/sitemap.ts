6dfabd3b58725facd627e0ab8b4b6b83a708cefc
CardanoBuyBot <tonscanner@outlook.com>
2026-03-18 23:36:49 -0300

    Update sitemap.ts

--- api/sitemap.ts
+++ api/sitemap.ts
@@ -21,7 +21,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
 
       return `
   <url>
-    <loc>https://cryptobot.ltd/blog/${a.slug}</loc>
+    <loc>https://www.cryptobot.ltd/blog/${a.slug}</loc>
     <lastmod>${date}</lastmod>
     <changefreq>weekly</changefreq>
     <priority>0.8</priority>
@@ -31,13 +31,13 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
-    <loc>https://cryptobot.ltd/</loc>
+    <loc>https://www.cryptobot.ltd/</loc>
     <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
     <changefreq>weekly</changefreq>
     <priority>1.0</priority>
   </url>
   <url>
-    <loc>https://cryptobot.ltd/blog</loc>
+    <loc>https://www.cryptobot.ltd/blog</loc>
     <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
     <changefreq>daily</changefreq>
     <priority>0.9</priority>
